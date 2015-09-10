36f1cee6adfc869436eb39ea038cff2ca64e6bd6
Liulijin <llj098@gmail.com>
2015-09-11 00:17:15 +0800

    first

---
+++ b/src/com/example/b/MyView.java
@@ -0,0 +1,18 @@
+package com.example.b;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by lj on 9/11/15.
+ */
+public class MyView extends View {
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
